b7ff99904807730675d7dda54170b508642a4839
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-04-03 23:55:14 +0530

    initialise

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import React from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className='min-h-screen w-screen bg-Primary'>
+      <h1 className='text-Heading'>hhiiii</h1>
+     
+    </div>
+    </>
+  )
+}
+
+export default App
